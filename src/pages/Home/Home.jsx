2b9a18e942f59e37d95cbf7bb774275946cbe58f
Humayun1318 <humayunKabir6267@gmail.com>
2025-03-19 01:44:44 +0600

    intial route setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+  return (
+    <div className="text-yellow">
+      this is from home page
+      
+    </div>
+  );
+};
+
+export default Home;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>Hello form world</h1>
+    element: <Home></Home>
   },
   {
     path: "/auth",
